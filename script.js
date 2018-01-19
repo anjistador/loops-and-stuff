171324e3fe4074dec47deb39b25ea3d63140c462
ANja <anjajokic24@gmail.com>
2018-01-19 14:49:07 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+console.log("Also great to meet you, + UserName")
+var UserName = prompt ("What is your name?");
+alert("Nice to meet you," + UserName);
+
+// var give you a pop up, for example
+// var this is a nikename= promot ("the questions");
+
+ // console.log show up on the consle
+// example
+// console.log("answer to your question"+nikename)
+
+// boolean
+// true or fales
+// Operator	Name	Example	​Result
+// >	Greater than	x > 10	false
+// >=	Greater than or equal to	x >= 5	true
+// <	Less than	x < -50	false
+// <=	Less than or equal to	x <= 100	true
+// ==	Equal to	x == "5"	true try to look for any similarty 
+// !=	Not equal to	x != "b"	true
+// ===	Equal value and type	x === "5"	false look for the similarity number and value need to be exact match
+// !==	Not equal value or equal type	x !== "5"	true
+
+// NaN= not a number
+
+// Logical Operators
+// AND, OR, and NOT
+// Operator	Name	Example	Result
+// &&	AND	x < 10 && x !== 5	false
+// ||	OR	y > 9 || x === 5	true
+// !	NOT	!(x === y)	true
+// Assuming x = 5 and y = 9
+
+// If -
+// Else IF-
+// Else-
+
+// typeof will tell what kind of stiring it is.
+
+// While loops- continue to run the code
+// exampl
+//var count = 1;
+
+//while( count < 6) {
+	console.log("count is: " + count); 
+	count++; 
+}
+
+
+
